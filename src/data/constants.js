712f649221298b3abdb061d0daf38155390a8f6e
kunalgupta06 <21f3001818@ds.study.iitm.ac.in>
2024-07-03 10:29:00 +0530

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,320 @@
+export const Bio = {
+  name: "Kunal Gupta",
+  roles: [
+    "Full Stack Developer",
+    "Data Scientist",
+    "Programmer",
+    "Data Analyst"
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/kunalgupta06/",
+  resume:
+    "https://docs.google.com/document/d/1KqG3hx-lz2zB9c0CNHaRe6QbpWN6a9fr/edit?usp=sharing&ouid=111889594562709790140&rtpof=true&sd=true",
+  linkedin: "https://www.linkedin.com/in/kunal-gupta-004427224?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  twitter: "https://twitter.com/RishavChanda",
+  insta: "https://www.instagram.com/imkunal.06/",
+  facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+      {
+        name: "Vue Js",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg",
+      },
+      {
+        name: "Angular Js",
+        image:
+          "https://media.licdn.com/dms/image/C4E12AQHu3FdpJSsccA/article-inline_image-shrink_1500_2232/0/1568013692165?e=1725494400&v=beta&t=QWvUjH5BDCgheZXpb-YOCI_ZJ70lJD8hemP6CCwGouk",
+      },
+      {
+        name: "React Js",
+        image:
+          "https://futurevisioncomputers.com/wp-content/uploads/2024/02/react.js-img-1.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "sqlite",
+        image: "https://www.isummation.com/wp-content/uploads/2022/09/SQLite.png",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://cdn.hashnode.com/res/hashnode/image/upload/v1615396992718/UBhOk2Nwz.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+      },
+      {
+        name: "C++",
+        image: "https://training.digigrowhub.in/wp-content/uploads/2021/02/do-coding-of-any-program-by-c-plus-plus-perfectly-and-within-time.jpg",
+      },
+    ],
+  },
+  {
+    title: "Data Science",
+    skills: [
+      {
+        name: "Machine Learning",
+        image:
+          "https://as2.ftcdn.net/v2/jpg/03/98/18/19/1000_F_398181949_BudYmmAeTPJwDz6HMxwf1PL3ZNIblohm.jpg",
+      },
+      {
+        name: "Data Analytics",
+        image:
+          "https://img.freepik.com/premium-vector/analysis-search-engine-optimization-seo-monitoring-icon-simple-editable-vector-graphics_922357-21524.jpg?w=740",
+      },
+      {
+        name: "Statistics",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/4/40/Fisher_iris_versicolor_sepalwidth.svg",
+      },
+      {
+        name: "Data Visualization",
+        image:
+          "https://treehousetechgroup.com/wp-content/uploads/2020/01/Screen-Shot-2020-01-28-at-10.13.28-AM.png",
+      },
+      {
+        name: "Power BI",
+        image:
+          "https://logos-world.net/wp-content/uploads/2022/02/Power-BI-Logo.png",
+      },
+      {
+        name: "Scikit-Learn",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://avatars.githubusercontent.com/u/18133?s=200&v=4",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "MS Excel",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/34/Microsoft_Office_Excel_%282019%E2%80%93present%29.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://a.fsdn.com/allura/s/postman/icon?1716797635?&w=135",
+      },
+     
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 1,
+    img: "https://logowik.com/content/uploads/images/bajaj-allianz-insurance6920.logowik.com.webp",
+    role: "SDE Intern at Bajaj Allianz Life Insurance Company(1 July 2024 - Present)",
+    
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/6/69/IIT_Madras_Logo.svg",
+    school: "Indian Institute of Technology, Madras",
+    date: "Sept 2021 - Sep 2025",
+    grade: "7.4 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Data Science and Programming at Indian Institute of Technology, Madras. I have completed Diploma level and currently enrolled in BSc level have a CGPA of 7.4. I am also founder of the Akord Music Society at IIT-M, where I am learning ,and working on my managerial and leadership skills with a team of talented Musicians and management enthusiasts and Leading them.",
+    degree: "Bachelor of Science - BS, Data Science and Programming",
+  },
+  {
+    id: 1,
+    img: "https://images.shiksha.com/mediadata/images/1652095604phpLTdf0U.jpeg",
+    school: "Bhagwan Parshuram Institute of Technology, Delhi",
+    date: "October 2021 - July 2025",
+    grade: "8.5 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Information Technology at Bhagwan Parshuram Institute of Technology, Delhi.It is affiliated by Guru Gobind Singh Indraprastha University(IPU).",
+    degree: "BTech, Information Technology",
+  },
+  {
+    id: 2,
+    img: "https://scontent.fdel10-1.fna.fbcdn.net/v/t39.30808-1/326206121_740217780606980_7981232513277526389_n.png?stp=c0.0.160.160a_dst-png_p160x160&_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=2n9q9WMYM7cQ7kNvgFVbxta&_nc_ht=scontent.fdel10-1.fna&oh=00_AYCYgPQ_Py6hT-5zf-QWNexHja2oW6DAL4-YKlQvDacloA&oe=6662A731",
+    school: "Birla Scool, Pilani",
+    date: "Apr 2017 - Apr 2019",
+    grade: "85%",
+    desc: "I completed my class 10 education at Birla School, Pilani. I was in school Swimming team and Participated and won in many Inter School Swimming Competitions and District Level Competitions",
+    degree: "CBSE(X), Foundation in Technology",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Online Shopping Mart",
+    date: "May 2023 - Jul 2023",
+    description:
+      "Online Shopping Mart This web application of the grocery shopping experience by enabling users to purchase groceries online while allowing sellers to efficiently manage their inventory.",
+    image:
+      "https://raw.githubusercontent.com/kunalgupta06/onlineshoppingmart/main/Screenshot%202023-10-25%20225519.png",
+    tags: [
+      "Python",
+      "HTML",
+      "CSS",
+      "Flask Framework",
+      " jinja",
+      "Bootstrap",
+      "SQLite",
+      
+    ],
+    category: "web app",
+    github: "https://github.com/kunalgupta06/onlineshoppingmart",
+    webapp: "https://github.com/kunalgupta06/onlineshoppingmart",
+  },
+  {
+    id: 0,
+    title: "Realtime Bus Tracking System",
+    date: "Oct 2023 - Nov 2023",
+    description:
+      "Real Time Bus Tracking System Real-Time Bus Tracking System is an innovative software solution designed to enhance the efficiency and convenience of public transportation. This system integrates advanced technologies, including GPS and Google Maps, to provide users with real-time information about bus locations, routes, and nearby bus stops. I handled Software Part of it.",
+    image:
+      "https://raw.githubusercontent.com/kunalgupta06/bustrackingsystem/main/back%20img1.jpg",
+    tags: ["JavaScript", "HTML", "CSS", "Python", "Flask Framework", "SQLite","Data Analysis","Machine Learning"],
+    category: "web app",
+    github: "https://github.com/kunalgupta06/bustrackingsystem",
+    webapp: "https://podstream.netlify.app/",
+    member: [
+      {
+        name: "Kunal Gupta",
+        img: "https://avatars.githubusercontent.com/u/128394074?s=400&u=f769db4594eaa6dffc81d0f503596564adbae031&v=4",
+        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/kunalgupta06/",
+      },
+      {
+        name: "Tushar Garg",
+        
+      },
+      {
+        name: "Abhigya Sharma",
+        
+      },
+      {
+        name: "Avinash Kumar",
+        
+      },
+      {
+        name: "Aditya Singh Dangi",
+        
+      },
+      {
+        name: "Shivam Kumar Singh",
+        
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Online Music Streaming Platform",
+    date: "April 2024 - Present",
+    description:
+    "Music Streaming App This is a music streaming platform, Here the one can register as Creator or Music Listener. This multi-user application serves as a dynamic platform for streaming music and reading lyrics, catering to both general users and content creators. Admins oversee the system, ensuring smooth operation and content management, while users can immerse themselves in a rich musical experience. They can play songs, read lyrics, rate their favorites, and create personalized playlists. Content creators have the capability to add new songs, albums, and lyrics, enriching the app's library.",
+    image:
+      "https://raw.githubusercontent.com/kunalgupta06/musicstreamingapp/main/Music%20Studio%20(1).png",
+    tags: [
+      "Vue Js",
+      "SQLite",
+      "Python",
+      "HTML",
+      "CSS",
+      "Jinja",
+      "Flask Framework",
+      "Data Analytics",
+      "JavaScript",
+      "Django",
+    ],
+    category: "web app",
+    github: "https://github.com/kunalgupta06/musicstreamingapp",
+    webapp: "https://github.com/kunalgupta06/musicstreamingapp",
+  },
+  {
+    id: 2,
+    title: "Business Data Management Project",
+    date: "Sept 2022 - Nov 2022",
+    description:
+      "This Business Data Management (BDM) Capstone Project focuses on SKU sales, inventory analysis, and customer retention for a Louis Phillipe garments outlet in Faridabad, Haryana. Analyzing data from April to October 2022, I utilized Excel functions like pivot tables and VLOOKUP's to identify top and bottom-performing SKUs and determine average monthly sales, aiding in strategic planning for optimal results.",
+    image:
+      "https://media.licdn.com/dms/image/D5612AQEqiyR9LLRJLg/article-cover_image-shrink_720_1280/0/1713939003771?e=1723075200&v=beta&t=FdyUWzEvTJFIz_swkox1cj7hz3sciqj9dpteUfUQH5M",
+    tags: ["Deriving business insights", "Microsoft Excel", "Financial Data Analytics","Data Visualisation","Power BI"],
+    category: "data analysis",
+    github: "https://github.com/kunalgupta06/businessdatamanagement",
+    webapp: "https://github.com/kunalgupta06/businessdatamanagement",
+   
+    
+  },
+  {
+    id: 8,
+    title: "Recipe Rating Prediction",
+    date: "March 2024",
+    description:
+    "This project focused on predicting food recipe ratings using diverse attributes like recipe names and user reviews. It encompasses datasets for model training and testing, alongside a sample submission file. The project explores columns detailing recipe placement, user information, and comment attributes, demonstrating analytical and predictive modeling skills.",
+    image:
+      "https://media.istockphoto.com/id/1203053489/photo/the-concept-of-cooking-meat-the-chef-cook-salt-on-the-cooked-steak-on-a-black-background-a.jpg?s=612x612&w=0&k=20&c=vOg4nRN_eW78kG1C3JZwkcBd88TQCjZFgnG2cjOvoxs=",
+    tags: ["Python", "Sklearn", "Natural Language Processing", "Random Forest Tree", "Naïve Bayes", "Django", "Jupiter Notebook"],
+    category: "machine learning",
+    github: "https://github.com/kunalgupta06/Recipe-Ratings-Prediction",
+    webapp: "https://github.com/kunalgupta06/Recipe-Ratings-Prediction",
+  },
+];
+
